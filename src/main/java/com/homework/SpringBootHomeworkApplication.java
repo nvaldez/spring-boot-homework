8d7c8fcbdee689419247b7cd13c2d6cb8ce6e607
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2020-05-18 10:19:18 -0500

    added html form and css

---
+++ src/main/java/com/homework/SpringBootHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHomeworkApplication.class, args);
+	}
+
+}
